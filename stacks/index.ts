c4622e2ce3af83ed90b6f4e857d3e69516f9bd54
Dade Sheng <dade@a.team>
2023-01-02 23:42:00 +0800

    remove all resources when non-prod stages are removed

--- stacks/index.ts
+++ stacks/index.ts
@@ -1,7 +1,12 @@
 import { App } from '@serverless-stack/resources';
+import { RemovalPolicy } from 'aws-cdk-lib';
 import { SchedulerStack } from './SchedulerStack';
 
 export default function (app: App) {
+  // Remove all resources when non-prod stages are removed
+  if (app.stage !== 'prod') {
+    app.setDefaultRemovalPolicy(RemovalPolicy.DESTROY);
+  }
   app.setDefaultFunctionProps({
     runtime: 'nodejs16.x',
     srcPath: 'services',
